2033e77854fc7906e4f0b1e9d79c469bfad1585b
LyanBrito <lyanbrito613@gmail.com>
2024-07-06 17:17:20 -0300

    refactor: refactored form script

--- src/script/recive-email.js
+++ src/script/recive-email.js
@@ -1,24 +1,21 @@
 import env from "../config/config";
 
-(function( ) {
-    emailjs.init({
-        publicKey: env.jS_APP_EMAILJS_PUBLICKEY,
-    })
+emailjs.init({
+  publicKey: env.jS_APP_EMAILJS_PUBLICKEY,
+});
 
-
-})();
-document.getElementById("contact-form").addEventListener("submit", function(e){
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (e) {
     e.preventDefault();
-    emailjs.sendForm(env.jS_APP_EMAILJS_SERVICEID, env.jS_APP_EMAILJS_TEMPLATEID_RECEIVED, this).then(
-        function(){
-            console.log('Sucesso')
-            alert('Email enviado com Sucesso')
 
-        }, function(err){
-            console.log('Erro', err)
-            alert('Falha ao enviar o email, tente novamente mais tarde.')
-        }
-
-    )
-
-})
+    emailjs
+      .sendForm(env.jS_APP_EMAILJS_SERVICEID, env.jS_APP_EMAILJS_TEMPLATEID_RECEIVED, this)
+      .then(() => {
+        console.log("Success!");
+        alert("Email enviado com sucesso");
+      }), (err) => {
+        console.log("Fail", err);
+        alert("Falha ao enviar o email, tente mais tarde!");
+      };
+  });
